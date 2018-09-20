4b041c528a3dd361ccfe184736635a0ddae930ce
Jalen.Jordan <jalenjordan55@yahoo.com>
2018-09-20 15:02:49 -0500

    added and modifyed certain elements

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+     $('.tabs a ').click(function(){
+          $('#commerical').fadeTo("slow",0.5, function(){
+               //Animation complete.
+          });
+     });
+});
